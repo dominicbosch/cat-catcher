5fb5d88d2817593b4615508e7075759d9f5d9bc9
Dominic Bosch <dominic.bosch@dobotech.ch>
2018-08-30 21:26:37 +0200

    eslint support added

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-let gulp = require('gulp');
-let clean = require('gulp-clean');
-let exec = require('child_process').exec;
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const exec = require('child_process').exec;
 
 gulp.task('default', ['build']);
 
 // Move all relevant sources to the build folder
-gulp.task('build', ['clean'], function () {
-	exec('', function (err, stdout, stderr) {
+gulp.task('build', ['clean'], function (cb) {
+	exec('',  function (err, stdout, stderr) {
 		console.log(stdout);
 		console.error(stderr);
 		cb(err);
