89969bdba2ac1f09138146da0a5eb515963b97b4
Dominic Bosch <dominic.bosch@dobotech.ch>
2018-09-12 15:43:17 +0200

    simplyfying the run script

--- runCatcherOnRaspi.sh
+++ runCatcherOnRaspi.sh
@@ -7,13 +7,6 @@ fi
 
 raspi=$1
 catdir="cat-catcher"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-echo "Copying resources to Raspi '${raspi}'"
-ssh pi@${raspi} "rm -rf ${catdir}"
-ssh pi@${raspi} "mkdir ${catdir}/src"
-ssh pi@${raspi} "cd ${catdir} && npm install pigpio"
-scp -rp ${DIR}/src/* pi@${raspi}:${catdir}/src
-
-echo "Running cat-catcher on Raspi '${raspi}'"
-ssh pi@${raspi} "${catdir}/src/runCatcher.sh"
+echo "Updating git repo on Raspi '${raspi}'"
+ssh pi@${raspi} "cd ${catdir} && git checkout master && git pull && gulp run"
